d6d116d241c7d077efb4d5e9d040d136c8680e95
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2013-01-24 10:08:40 +0100

    Added some documentation to UACHandler and UACProvider.

--- JsdpuUACHandler/Program.cs
+++ JsdpuUACHandler/Program.cs
@@ -6,14 +6,35 @@ using System.IO;
 using System.IO.Pipes;
 using System.Linq;
 
+/// <summary>
+/// Runs commands with elevation via UACPerformer.exe.
+/// </summary>
 namespace UACHandler
 {
     class Executor
     {
+        /// <summary>
+        /// Identifier used for communication with UACPerformer via NamedPipes. 
+        /// </summary>
         private static int identifier = 0;
 
+        /// <summary>
+        /// Runs passed commands with elevation and redirects results on standard Out and standard Error.
+        /// </summary>
+        /// 
+        /// Results will be displayed for for each program sequentially: out1, err1, out2, err2...
+        /// 
+        /// <param name="commands">
+        /// Array of commands where each of them is a string containing command in the same form it would
+        /// have if run on console directly (program name and arguments separated by spaces).
+        /// </param>
         static void Main(string[] commands)
         {
+            if (commands.Length == 0)
+            {
+                return;
+            }
+
             ProcessStartInfo psInfo = new ProcessStartInfo();
             psInfo.FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UACPerformer.exe");
             psInfo.Arguments = parseArguments(commands);
@@ -27,19 +48,14 @@ namespace UACHandler
 
                 do
                 {
-
                     NamedPipeServerStream outServer = new NamedPipeServerStream(getIdentifier() + ".out", PipeDirection.In);
                     outServer.WaitForConnection();
-                    StreamReader outReader = new StreamReader(outServer);
-                    Console.Out.Write(outReader.ReadToEnd());
-                    outReader.Close();
+                    redirect(outServer, Console.Out);
                     outServer.Close();
 
                     NamedPipeServerStream errServer = new NamedPipeServerStream(getIdentifier() + ".err", PipeDirection.In);
                     errServer.WaitForConnection();
-                    StreamReader errReader = new StreamReader(errServer);
-                    Console.Error.Write(errReader.ReadToEnd());
-                    errReader.Close();
+                    redirect(errServer, Console.Error);
                     errServer.Close();
 
                     System.Threading.Thread.Sleep(500);
@@ -52,20 +68,58 @@ namespace UACHandler
             }
         }
 
-        static int getIdentifier() {
+        /// <summary>
+        /// Identifier used for communication with UACPerformer via NamedPipes. 
+        /// </summary>
+        /// <returns>
+        /// value of an identifier
+        /// </returns>
+        private static int getIdentifier() {
             Random random = new Random();
             while (identifier == 0)
+            {
                 identifier = random.Next(Int16.MaxValue);
+            }
             return identifier;
         }
 
-        static string parseArguments(string[] commands) {
+        /// <summary>
+        /// Prepares command that will run UACPerformer.exe.
+        /// </summary>
+        /// <param name="commands">
+        /// commands passed from Main(string[])
+        /// </param>
+        /// <returns>
+        /// command to perform
+        /// </returns>
+        private static string parseArguments(string[] commands) {
             List<string> args = new List<string>();
             args.Add(getIdentifier().ToString());
-            foreach (string command in commands) {
+            foreach (string command in commands)
+            {
                 args.Add("\"" + command.Replace('"', '\"') + "\"");
             }
             return String.Join(" ", args.ToArray());
+        }
+
+        /// <summary>
+        /// Redirects input from NamedPipe to output.
+        /// </summary>
+        /// <param name="_in">
+        /// incoming NamedPipe
+        /// </param>
+        /// <param name="_out">
+        /// outgoing Console writer
+        /// </param>
+        private static void redirect(NamedPipeServerStream _in, TextWriter _out)
+        {
+            StreamReader reader = new StreamReader(_in);
+            string tmp;
+            while ((tmp = reader.ReadLine()) != null)
+            {
+                _out.WriteLine(tmp);
+            }
+            reader.Close();
         }
     }
 }
--- JsdpuUACProvider/Program.cs
+++ JsdpuUACProvider/Program.cs
@@ -5,13 +5,28 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
 
-/**
- * 
- */
+/// <summary>
+/// Performs commands passed from UACHandler with elevation..
+/// </summary>
 namespace UACPerformer {
     class Executor {
+        /// <summary>
+        /// Identifier used for communication with UACPerformer via NamedPipes. 
+        /// </summary>
         private static string identifier;
 
+        /// <summary>
+        /// Runs passed commands with elevation and redirects results on standard Out and standard Error.
+        /// </summary>
+        /// 
+        /// Results will be displayed for for each program sequentially: out1, err1, out2, err2...
+        /// 
+        /// <param name="commands">
+        /// Array of commands where each of them is a string containing command in the same form it would
+        /// have if run on console directly (program name and arguments separated by spaces).
+        /// 
+        /// The first string though is identifier used for communication with UACHandler.exe.
+        /// </param>
         static void Main(string[] commands) {
             if (commands.Length == 0)
             {
@@ -26,13 +41,27 @@ namespace UACPerformer {
             }
         }
 
+        /// <summary>
+        /// Prepares command to run and executes it.
+        /// </summary>
+        /// <param name="command">
+        /// command passed from Main(string[])
+        /// </param>
         private static void parseCommand(String command) {
             ArgumentsHandler argumentsHandler = new ArgumentsHandler(command);
 
             if (argumentsHandler.ParseArguments())
+            {
                 runCommand(argumentsHandler);
+            }
         }
 
+        /// <summary>
+        /// Executes command.
+        /// </summary>
+        /// <param name="argumentsHandler">
+        /// handler with a program to run
+        /// </param>
         private static void runCommand(ArgumentsHandler argumentsHandler) {
             ProcessStartInfo psInfo = new ProcessStartInfo();
             psInfo.FileName = argumentsHandler.Program;
@@ -50,16 +79,12 @@ namespace UACPerformer {
 
                 NamedPipeClientStream outClient = new NamedPipeClientStream(".", identifier + ".out", PipeDirection.Out);
                 outClient.Connect();
-                StreamWriter outWriter = new StreamWriter(outClient);
-                outWriter.Write(process.StandardOutput.ReadToEnd());
-                outWriter.Close();
+                redirect(process.StandardOutput, outClient);
                 outClient.Close();
 
                 NamedPipeClientStream errClient = new NamedPipeClientStream(".", identifier + ".err", PipeDirection.Out);
                 errClient.Connect();
-                StreamWriter errWriter = new StreamWriter(errClient);
-                errWriter.Write(process.StandardError.ReadToEnd());
-                errWriter.Close();
+                redirect(process.StandardError, errClient);
                 errClient.Close();
             }
             catch (Win32Exception ex)
@@ -67,29 +92,81 @@ namespace UACPerformer {
                 Console.Error.WriteLine(ex.Message);
             }
         }
+
+        /// <summary>
+        /// Redirects output to NamedPipe.
+        /// </summary>
+        /// <param name="_in">
+        /// incoming Console reader
+        /// </param>
+        /// <param name="_out">
+        /// outgoing NamedPipe
+        /// </param>
+        private static void redirect(StreamReader _in, NamedPipeClientStream _out)
+        {
+            StreamWriter writer = new StreamWriter(_out);
+            string tmp;
+            while ((tmp = _in.ReadLine()) != null)
+            {
+                writer.WriteLine(tmp);
+            }
+            writer.Close();
+        }
     }
 
+    /// <summary>
+    /// Handles arguments passed from Main(string[])
+    /// </summary>
     internal class ArgumentsHandler
     {
+        /// <summary>
+        /// Contains original command.
+        /// </summary>
         private String command;
+
+        /// <summary>
+        /// Contains program's name.
+        /// </summary>
         private String program;
+
+        /// <summary>
+        /// Contains program's arguments.
+        /// </summary>
         private String arguments;
 
+        /// <summary>
+        /// Initiates handler with command.
+        /// </summary>
+        /// <param name="command">
+        /// command to parse
+        /// </param>
         public ArgumentsHandler(string command)
         {
             this.command = command;
         }
 
+        /// <summary>
+        /// Returns program's name.
+        /// </summary>
         public String Program
         {
             get { return program; }
         }
 
+        /// <summary>
+        /// Returns program's arguments.
+        /// </summary>
         public String Arguments
         {
             get { return arguments; }
         }
 
+        /// <summary>
+        /// Parses command and returns whether there is any program to run.
+        /// </summary>
+        /// <returns>
+        /// whether command contained any program's name
+        /// </returns>
         public bool ParseArguments()
         {
             string[] argumentsArray = command.Split(' ');
