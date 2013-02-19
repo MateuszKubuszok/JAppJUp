0fbb3287cad9f18563711dd80bc6227996c93948
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2013-02-19 14:10:03 +0100

    Changed routing for running program from menu.

--- AutoUpdaterGuiClient/src/com/autoupdater/gui/adapters/runnables/RunCommand.java
+++ AutoUpdaterGuiClient/src/com/autoupdater/gui/adapters/runnables/RunCommand.java
@@ -1,12 +1,15 @@
 package com.autoupdater.gui.adapters.runnables;
 
 import static com.autoupdater.gui.window.EInfoTarget.ALL;
+import static javax.swing.JOptionPane.*;
+import static net.jsdpu.logger.Logger.getLogger;
 import static net.jsdpu.process.executors.Commands.convertMultipleConsoleCommands;
 
 import java.io.IOException;
 import java.util.SortedSet;
 
 import net.jsdpu.EOperatingSystem;
+import net.jsdpu.logger.Logger;
 import net.jsdpu.process.executors.InvalidCommandException;
 
 import com.autoupdater.client.environment.settings.ProgramSettings;
@@ -15,6 +18,8 @@ import com.autoupdater.client.models.Update;
 import com.autoupdater.gui.adapters.Gui2ClientAdapter;
 
 public class RunCommand implements Runnable {
+    private static final Logger logger = getLogger(RunCommand.class);
+
     private final Gui2ClientAdapter adapter;
 
     private final ProgramSettings programSettings;
@@ -28,12 +33,49 @@ public class RunCommand implements Runnable {
 
     @Override
     public void run() {
-        if (isAnyUpdateAvailable()) {
+        logger.info("Starting up program: " + program);
+
+        if (!isUpdateDataChecked()) {
+            logger.debug("Starting up before update fetch completed - asking for decision");
+            int choice = showConfirmDialog(
+                    adapter.clientWindow(),
+                    "Updater hasn't yet fetched data from repositories. Would You like to run program without cheking updates?",
+                    "Updates not fetched", YES_NO_OPTION);
+            switch (choice) {
+            default:
+            case YES_OPTION:
+                logger.debug("Continue running");
+                break;
+            case NO_OPTION:
+                logger.debug("Cancel running");
+                return;
+            }
+        } else if (isAnyUpdateAvailable()) {
+            logger.debug("Installing updates before running");
+
             installUpdates();
 
-            // check whether update succeeded
+            if (hasInstallationSucceded()) {
+                logger.debug("Update installation succeded - continue running");
+            } else {
+                logger.debug("Update installation failed - asking for decision");
+                int choice = showConfirmDialog(
+                        adapter.clientWindow(),
+                        "Update installation failed. Do You wish to start up outdated program anyway?",
+                        "Updates not installed", YES_NO_OPTION);
+                switch (choice) {
+                default:
+                case YES_OPTION:
+                    logger.debug("Continue running");
+                    break;
+                case NO_OPTION:
+                    logger.debug("Cancel running");
+                    return;
+                }
+            }
         }
 
+        logger.debug("Calling start up command: " + programSettings.getPathToProgram());
         try {
             adapter.windowOperations().reportInfo(programSettings.getProgramName(),
                     programSettings.getProgramName() + " is starting up", ALL);
@@ -41,9 +83,14 @@ public class RunCommand implements Runnable {
                     .execute(convertMultipleConsoleCommands(programSettings.getPathToProgram()))
                     .rewind();
         } catch (IOException | InvalidCommandException e) {
+            logger.error("Start up failed", e);
             adapter.windowOperations().reportWarning(e.toString(),
                     programSettings.getProgramName(), ALL);
         }
+    }
+
+    private boolean isUpdateDataChecked() {
+        return adapter.dataStorage().isInitiated();
     }
 
     private boolean isAnyUpdateAvailable() {
@@ -52,10 +99,19 @@ public class RunCommand implements Runnable {
 
     private void installUpdates() {
         adapter.installationUtils().installUpdatesForProgram(program);
+        try {
+            adapter.installationUtils().getCurrentInstallationThread().join();
+        } catch (InterruptedException e) {
+            logger.warning("Update thread interrupter", e);
+        }
     }
 
     private boolean hasInstallationSucceded() {
         SortedSet<Update> updates = adapter.dataStorage().getUpdatesForProgram(program);
+        for (Update update : updates) {
+            if (update.getStatus().isInstallationFailed())
+                return false;
+        }
         return true;
     }
 }
