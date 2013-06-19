8d1191da2ab3a3ce7aee8eeb98b08bf0bceb09b2
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2013-06-19 15:04:10 +0200

    Name fix.

--- AutoUpdaterGuiClient/src/com/autoupdater/gui/config/GuiConfiguration.java
+++ AutoUpdaterGuiClient/src/com/autoupdater/gui/config/GuiConfiguration.java
@@ -19,7 +19,7 @@ import java.awt.Dimension;
 import java.awt.Rectangle;
 
 public class GuiConfiguration {
-    public static String WINDOW_TITLE = "Manage AutoUpdater";
+    public static String WINDOW_TITLE = "JAppJUp - Auto Updater";
 
     public static Rectangle WINDOW_BOUNDS = new Rectangle(50, 50, 800, 500);
     public static Dimension WINDOW_MIN_SIZE = new Dimension(800, 500);
